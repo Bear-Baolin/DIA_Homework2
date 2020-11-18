c4c27cfd206194f280161edb072929e704958940
宝霖 熊 <2278526090@qq.com>
2020-11-18 16:11:28 +0800

    拉取文件选择框选择文件

---
+++ DIA_homework2/DIA_homework2.cpp
@@ -0,0 +1,57 @@
+﻿// DIA_homework2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <Windows.h>
+#include <commdlg.h>
+#include <ShlObj.h>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+char* wcharTochar(const wchar_t* _wchar)
+{
+	char* _char;
+	int len = WideCharToMultiByte(CP_ACP, 0, _wchar, (int)wcslen(_wchar), NULL, 0, NULL, NULL);
+	_char = new char[len + 1];
+	WideCharToMultiByte(CP_ACP, 0, _wchar, (int)wcslen(_wchar), _char, len, NULL, NULL);
+	_char[len] = '\0';
+	return _char;
+}
+
+int main()
+{
+	TCHAR szBuffer[MAX_PATH] = { 0 };
+	OPENFILENAME ofn = { 0 };
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = NULL;
+	ofn.lpstrFile = szBuffer;
+	ofn.lpstrFile[0] = { '\0' };
+	ofn.nMaxFile = sizeof(szBuffer);
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = L"C:\\Program Files";
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	if (GetOpenFileName(&ofn))
+	{
+		wchar_t *t = ofn.lpstrFile;
+		cout << wcharTochar(t) << endl;
+		
+	}
+	return 0;
+   // image = imread();//读取原图像
+    
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
